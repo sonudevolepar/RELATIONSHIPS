a09bffd8bef24ca485ea65778ea663885600cdea
sonukumar <sonu9508@gmail.com>
2026-02-25 22:28:22 +0530

    addingfuction for delcust

--- models/customer.js
+++ models/customer.js
@@ -1,33 +1,68 @@
 const mongoose = require("mongoose");
+const { Schema } = mongoose;
 
-// connect to MongoDB
-mongoose.connect("mongodb://127.0.0.1:27017/relationDemo", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+main()
+  .then(() => console.log("Connection successful"))
+  .catch((err) => console.log(err));
+
+async function main() {
+  await mongoose.connect("mongodb://127.0.0.1:27017/relationDemo");
+}
+
+const orderSchema = new Schema({
+  username: String,
+  email: String,
 });
 
-const Customer = require("./models/customer");
-const Order = require("./models/order");
-
-const addCust = async () => {
-  try {
-    const newOrder = new Order({ item: "chips", price: 20 });
-    await newOrder.save();
-
-    const newCust = new Customer({
-      name: "karan Arjun",
-      orders: [newOrder._id]
-    });
-
-    await newCust.save();
-
-    console.log("Added new customer with order:", newCust);
-
-  } catch (err) {
-    console.error(err);
-  } finally {
-    mongoose.connection.close();
+const customerSchema = new Schema({
+  name: String,
+  orders: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Order",
+    },
+  ],
+});
+// customerSchema.pre("findOneAndDelete", async () => {
+//   console.log("PRE MIDDLEWARE");
+// });
+customerSchema.post("findOneAndDelete", async (customer) => {
+  if(customer.orders.length) {
+    let res = await Order.deleteMany({ _id: { $in: customer.orders } });
+    console.log(res);
   }
+
+});
+
+const Order = mongoose.model("Order", orderSchema);
+const Customer = mongoose.model("customer", customerSchema);
+
+//functions
+const findCustomer  = async () => {
+  let result = await Customer.find({}).populate("orders");
+  console.log(result[0]);
 };
 
-addCust();
+  const addCust = async () => {
+    let newCust = new Customer({
+      name: "karan Arjun",
+    });
+    
+    let newOrder = new Order({
+      item:"Burger",
+      price: 250,
+    });
+    
+    newCust.orders.push(newOrder);
+    
+    await newOrder.save();
+    await newCust.save();
+    console.log("added new customer");
+  };
+ const delCust = async () => {
+  let data = await Customer.findByIdAndDelete("6999f257c3431bd7bfb775b5");
+  console.log(data);
+ };
+
+ //addCust();
+ delCust();
